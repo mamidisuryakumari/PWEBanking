6dbf123e4bf849ae03031137dbc886f0205ef4ff
unknown <kumari.mudile@gmail.com>
2026-02-11 14:45:59 +0530

    modified config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,11 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries:1,
+  workers: 1,
+  //forbidOnly: !!process.env.CI,
+  //retries: process.env.CI ? 2 : 0,
+  //workers: process.env.CI ? 1 : undefined,
   reporter: [
     ['html'],
     ['allure-playwright', { outputFolder: 'allure-results' }]
